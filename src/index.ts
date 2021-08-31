b531d8225d00c8002237b54eb7912a2f520b7f28
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-08-30 23:08:45 -0700

    refactored object composition

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+//create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+//create an instance of matchReader and pass in something stisfying the 'dataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (
     (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
     (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
